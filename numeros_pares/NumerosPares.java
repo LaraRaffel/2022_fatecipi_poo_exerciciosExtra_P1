628a7ecb4cf06399ced4eec84a04d4f7e87d8dfb
Lara Raffel <lara.b.r@outlook.com>
2022-11-26 17:24:59 -0300

    Exercício 05_NumerosPares finalizado

---
+++ numeros_pares/NumerosPares.java
@@ -0,0 +1,9 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+    
+    //declaração dos números
+    for (int i=0; i<100;i++){
+        if(i%2 == 0){System.out.printf(" %d -", i);}
+      }
+    }
+  }
